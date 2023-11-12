3a6c5a2eb68e08d9af0708e8349a7bb32aa8b4ec
rohtemp01 <roh1902@ajou.ac.kr>
2023-11-12 15:53:51 +0900

    카메라 기능 구현중

--- app/src/main/java/com/example/mp_app/L2_Main/CameraXAct.java
+++ app/src/main/java/com/example/mp_app/L2_Main/CameraXAct.java
@@ -1,17 +1,22 @@
 package com.example.mp_app.L2_Main;
 
+import static android.media.MediaRecorder.VideoSource.CAMERA;
+
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.camera.core.Camera;
 import androidx.camera.core.CameraSelector;
+import androidx.camera.core.ImageCapture;
 import androidx.camera.core.Preview;
 import androidx.camera.lifecycle.ProcessCameraProvider;
 import androidx.camera.view.PreviewView;
+import androidx.core.app.ActivityCompat;
 import androidx.core.content.ContextCompat;
 import androidx.lifecycle.LifecycleOwner;
 
 import com.google.common.util.concurrent.ListenableFuture;
 
+import android.content.pm.PackageManager;
 import android.os.Bundle;
 
 import com.example.mp_app.R;
@@ -21,17 +26,31 @@ import java.util.concurrent.ExecutionException;
 public class CameraXAct extends AppCompatActivity {
 
     PreviewView previewView;//null ptr error
+    private ImageCapture imageCapture;
+
     private ListenableFuture<ProcessCameraProvider> cameraProviderFuture;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.act_camera);
+
+        if (ContextCompat.checkSelfPermission(this, android.Manifest.permission.CAMERA ) != PackageManager.PERMISSION_GRANTED) {
+            ActivityCompat.requestPermissions(this, new String[] {android.Manifest.permission.CAMERA}, CAMERA);
+        }
+
         previewView = findViewById(R.id.previewView);
         cameraProviderFuture = ProcessCameraProvider.getInstance(this);
         cameraProviderFuture.addListener(() -> {
             try {
                 ProcessCameraProvider cameraProvider = cameraProviderFuture.get();
+
+                Preview preview = new Preview.Builder().build();
+
+                imageCapture = new ImageCapture.Builder()
+                        .setCaptureMode(ImageCapture.CAPTURE_MODE_MINIMIZE_LATENCY)
+                        .build();
+
                 bindPreview(cameraProvider);
             } catch (ExecutionException | InterruptedException e) {
                 // No errors need to be handled for this Future.
