b9e91844d850f20bd279f5ef87531b1250bc7203
rohtemp01 <roh1902@ajou.ac.kr>
2023-11-25 19:12:02 +0900

    fragment - 스와이프 기능 구현중 cameraX - fragment로 구현 완료

---
+++ app/src/main/java/com/example/mp_app/Tools/RecyclerView/Chat_Data.java
@@ -0,0 +1,6 @@
+package com.example.mp_app.Tools.RecyclerView;
+
+public class Chat_Data {
+    public String user_num;
+    public boolean focused = false;
+}
--- app/src/main/java/com/example/mp_app/Tools/RecyclerView/RecyclerAdapter.java
+++ app/src/main/java/com/example/mp_app/Tools/RecyclerView/RecyclerAdapter.java
@@ -14,23 +14,39 @@ import com.example.mp_app.R;
 import java.util.ArrayList;
 
 public class RecyclerAdapter extends RecyclerView.Adapter<RecyclerAdapter.ViewHolder>{
-    final static int MY_MESSAGE = 0;
-    final static int YOUR_MESSAGE = 1;
+    final static int CHAT_SELF = 0;
+    final static int CHAT_RECEIVED = 1;
     final static int FOCUSED_MESSAGE = 2;
-    ArrayList<String> recyclerData = new ArrayList<>();
+    ArrayList<Chat_Data> recyclerData = new ArrayList<>();
     @NonNull
     @Override
     public ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
         //1. viewType 따라서 viewHolder 생성
         View view;
         switch(viewType){
-            case MY_MESSAGE:{
-                view = LayoutInflater.from(parent.getContext()).inflate(R.layout.aaaa, parent, false);
+            case FOCUSED_MESSAGE:{
+
             }
-            case YOUR_MESSAGE:{
-                view = LayoutInflater.from(parent.getContext()).inflate(R.layout.aaaa, parent, false);
+            case CHAT_SELF:{
+                view = LayoutInflater.from(parent.getContext()).inflate(R.layout.chatbox_self, parent, false);
+                return new ViewHolder(view);
+            }
+            case CHAT_RECEIVED:{
+                view = LayoutInflater.from(parent.getContext()).inflate(R.layout.chatbox_received, parent, false);
             }
             default: return null;
+        }
+    }
+    @Override
+    public int getItemViewType(int position){
+        switch(recyclerData.get(position).user_num){
+            case recyclerData.get(position).focused == true: {
+                return FOCUSED_MESSAGE;
+            }
+            case PreferenceUtil.getStringValue(context,"myNum"):{
+                return CHAT_SELF;
+            }
+            default: return CHAT_RECEIVED;
         }
     }
 
