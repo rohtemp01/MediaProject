fb989fcc7d2bf1db9a9a03367d0d6ce6c5e270dd
HYEONSEOB4726 <147833334+HYEONSEOB4726@users.noreply.github.com>
2023-11-26 14:05:05 +0900

    .

--- app/src/main/java/com/example/mp_app/L1_Intro/SignInAct.java
+++ app/src/main/java/com/example/mp_app/L1_Intro/SignInAct.java
@@ -1,16 +1,59 @@
 package com.example.mp_app.L1_Intro;
 
+import static android.content.ContentValues.TAG;
+
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
 import com.example.mp_app.R;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 
 public class SignInAct extends AppCompatActivity {
+    private FirebaseAuth mAuth;
+    private EditText editTextEmail, editTextPassword;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_sign_in);
+
+        mAuth = FirebaseAuth.getInstance();
+        editTextEmail = findViewById(R.id.editTextEmail);
+        editTextPassword = findViewById(R.id.editTextPassword);
+        Button buttonSignIn = findViewById(R.id.buttonSignIn);
+
+        buttonSignIn.setOnClickListener(v -> {
+            String email = editTextEmail.getText().toString();
+            String password = editTextPassword.getText().toString();
+            signIn(email, password);
+        });
+    }
+
+    private void signIn(String email, String password) {
+        mAuth.signInWithEmailAndPassword(email, password)
+                .addOnCompleteListener(this, task -> {
+                    if (task.isSuccessful()) {
+                        FirebaseUser user = mAuth.getCurrentUser();
+                        Log.d(TAG, "로그인 성공: " + user.getEmail());
+                        Toast.makeText(SignInAct.this, "로그인되었습니다.", Toast.LENGTH_SHORT).show();
+                        startCoupleActivity();
+                    } else {
+                        Log.w(TAG, "로그인 실패", task.getException());
+                        Toast.makeText(SignInAct.this, "로그인에 실패했습니다. 다시 시도해주세요.", Toast.LENGTH_SHORT).show();
+                    }
+                });
+    }
+
+    private void startCoupleActivity() {
+        Intent intent = new Intent(SignInAct.this, CoupleAct.class);
+        startActivity(intent);
+        finish();
     }
 }
--- app/src/main/java/com/example/mp_app/L1_Intro/SignUpAct.java
+++ app/src/main/java/com/example/mp_app/L1_Intro/SignUpAct.java
@@ -1,16 +1,59 @@
 package com.example.mp_app.L1_Intro;
 
+import static android.content.ContentValues.TAG;
+
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
 import com.example.mp_app.R;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 
 public class SignUpAct extends AppCompatActivity {
+    private FirebaseAuth mAuth;
+    private EditText editTextEmail, editTextPassword;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_sign_up);
+
+        mAuth = FirebaseAuth.getInstance();
+        editTextEmail = findViewById(R.id.editTextEmail);
+        editTextPassword = findViewById(R.id.editTextPassword);
+        Button buttonSignUp = findViewById(R.id.buttonSignUp);
+
+        buttonSignUp.setOnClickListener(v -> {
+            String email = editTextEmail.getText().toString();
+            String password = editTextPassword.getText().toString();
+            signUp(email, password);
+        });
+    }
+
+    private void signUp(String email, String password) {
+        mAuth.createUserWithEmailAndPassword(email, password)
+                .addOnCompleteListener(this, task -> {
+                    if (task.isSuccessful()) {
+                        FirebaseUser user = mAuth.getCurrentUser();
+                        Log.d(TAG, "회원 가입 성공: " + user.getEmail());
+                        Toast.makeText(SignUpAct.this, "회원 가입이 완료되었습니다.", Toast.LENGTH_SHORT).show();
+                        startCoupleActivity();
+                    } else {
+                        Log.w(TAG, "회원 가입 실패", task.getException());
+                        Toast.makeText(SignUpAct.this, "회원 가입에 실패했습니다. 다시 시도해주세요.", Toast.LENGTH_SHORT).show();
+                    }
+                });
+    }
+
+    private void startCoupleActivity() {
+        Intent intent = new Intent(SignUpAct.this, CoupleAct.class);
+        startActivity(intent);
+        finish();
     }
 }
